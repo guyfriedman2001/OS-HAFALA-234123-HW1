4701d2cc13f60a4ea2c83d0efe9fc2e4bdf4dfef
guy friedman <guy.friedman2001@gmail.com>
2025-04-16 15:23:00 +0300

    update shell exec loop to account for prompt changes

--- skeleton_smash/smash.cpp
+++ skeleton_smash/smash.cpp
@@ -12,11 +12,12 @@ int main(int argc, char *argv[]) {
 
 
     SmallShell &smash = SmallShell::getInstance();
-    while (true) {
-        std::cout << "smash> ";
+    while (true) { //FIXME: check if its ok to use fstream here or if we need to use C utils for this
+        std::cout << smash.getPrompt() << smash.getEndStr();
         std::string cmd_line;
         std::getline(std::cin, cmd_line);
         smash.executeCommand(cmd_line.c_str());
+        std::cout << std::endl;
     }
     return 0;
 }
