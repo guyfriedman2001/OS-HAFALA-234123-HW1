72b835110ee4973984aab0dd7634630bd898c725
student <guy.friedman2001@gmail.com>
2025-05-06 16:18:30 +0000

    "first_commit_from_smash_on_vm"

---
+++ skeleton_smash/cmake-build-test_local/CMakeFiles/skeleton_smash.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/skeleton_smash.dir/AliasManager.cpp.o"
+  "CMakeFiles/skeleton_smash.dir/AliasManager.cpp.o.d"
+  "CMakeFiles/skeleton_smash.dir/BuiltInCommands.cpp.o"
+  "CMakeFiles/skeleton_smash.dir/BuiltInCommands.cpp.o.d"
+  "CMakeFiles/skeleton_smash.dir/CommandFactories.cpp.o"
+  "CMakeFiles/skeleton_smash.dir/CommandFactories.cpp.o.d"
+  "CMakeFiles/skeleton_smash.dir/Commands.cpp.o"
+  "CMakeFiles/skeleton_smash.dir/Commands.cpp.o.d"
+  "CMakeFiles/skeleton_smash.dir/ExternalCommands.cpp.o"
+  "CMakeFiles/skeleton_smash.dir/ExternalCommands.cpp.o.d"
+  "CMakeFiles/skeleton_smash.dir/JobList.cpp.o"
+  "CMakeFiles/skeleton_smash.dir/JobList.cpp.o.d"
+  "CMakeFiles/skeleton_smash.dir/SmallShell.cpp.o"
+  "CMakeFiles/skeleton_smash.dir/SmallShell.cpp.o.d"
+  "CMakeFiles/skeleton_smash.dir/SpecialCommands.cpp.o"
+  "CMakeFiles/skeleton_smash.dir/SpecialCommands.cpp.o.d"
+  "CMakeFiles/skeleton_smash.dir/signals.cpp.o"
+  "CMakeFiles/skeleton_smash.dir/signals.cpp.o.d"
+  "CMakeFiles/skeleton_smash.dir/smash.cpp.o"
+  "CMakeFiles/skeleton_smash.dir/smash.cpp.o.d"
+  "skeleton_smash"
+  "skeleton_smash.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/skeleton_smash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
