a7f667f5f12e271b1ba571f5006d219bc0957c09
guy friedman <guy.friedman2001@gmail.com>
2025-05-06 18:25:24 +0300

    cyka blyat 2

---
+++ skeleton_smash/Temp_Restore.h
@@ -0,0 +1,17 @@
+//
+// Created by Guy Friedman on 06/05/2025.
+//
+
+#ifndef TEMP_RESTORE_H
+#define TEMP_RESTORE_H
+
+#if 0
+class Temp_Restore {
+public:
+  Temp_Restore();
+  ~Temp_Restore();
+};
+
+#endif
+
+#endif //TEMP_RESTORE_H
